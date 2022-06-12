73fa86eb6884a7a3b5b99fe127275cf4f537d3a2
Mutai-Gilbert <mutaigilbert85@gmail.com>
2022-06-12 08:23:18 +0300

    daily dose

---
+++ doc-string.py
@@ -0,0 +1,27 @@
+def readable_timedelta(days):
+    """Return a string of the number of weeks and days included in days."""
+    weeks = days // 7
+    remainder = days % 7
+    return "{} week(s) and {} day(s)".format(weeks, remainder)
+def readable_timedelta(days):
+    """Return a string of the number of weeks and days included in days.
+
+    Args:
+        days (int): number of days to convert
+    """
+    weeks = days // 7
+    remainder = days % 7
+    return "{} week(s) and {} day(s)".format(weeks, remainder)
+def readable_timedelta(days):
+    """
+    Return a string of the number of weeks and days included in days.
+
+    Parameters:
+    days -- number of days to convert (int)
+
+    Returns:
+    string of the number of weeks and days included in days
+    """
+    weeks = days // 7
+    remainder = days % 7
+    return "{} week(s) and {} day(s)".format(weeks, remainder)
